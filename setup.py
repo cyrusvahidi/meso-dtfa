8cdb285b1982d9c2a244708c8740f555d09b4bff
Cyrus Vahidi <10761938+cyrusvahidi@users.noreply.github.com>
2023-07-06 22:56:03 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,4 +1,17 @@
-import setuptools
+from setuptools import setup
 
-if __name__ == "__main__":
-    setuptools.setup(name="meso_dtfa")
+setup(
+    name="meso_dtfa",
+    version="0.1",
+    description="",
+    author="Cyrus Vahidi",
+    author_email="c.vahidi@qmul.ac.uk",
+    include_package_data=True,
+    packages=['meso_dtfa'],
+    url="https://github.com/cyrusvahidi/meso_dtfa-gpu",
+    install_requires=["gin-config",
+                      "librosa", 
+                      "torch",
+                      "tqdm",
+                      "fire"]
+)
